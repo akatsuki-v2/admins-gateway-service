c293dc4e1eabcbea4e58ae55b471bf5846c487d6
cmyui <cmyuiosu@gmail.com>
2022-10-24 11:23:14 -0400

    feat: get all & delete account endpoints

--- mount/app/api/rest/v1/accounts.py
+++ mount/app/api/rest/v1/accounts.py
@@ -2,6 +2,7 @@ from app.api.rest.context import RequestContext
 from app.api.rest.gateway import authenticate
 from app.api.rest.gateway import forward_request
 from app.api.rest.responses import Success
+from app.models import Status
 from app.models.accounts import Account
 from app.models.accounts import AccountUpdate
 from app.models.accounts import SignupForm
@@ -17,7 +18,6 @@ SERVICE_URL = "http://users-service"
 oauth2_scheme = HTTPBearer()
 
 
-# https://osuakatsuki.atlassian.net/browse/V2-10
 @router.post("/v1/accounts", response_model=Success[Account])
 async def sign_up(args: SignupForm, ctx: RequestContext = Depends()):
     response = await forward_request(ctx,
@@ -27,7 +27,18 @@ async def sign_up(args: SignupForm, ctx: RequestContext = Depends()):
     return response
 
 
-# https://osuakatsuki.atlassian.net/browse/V2-58
+@router.get("/v1/accounts", response_model=Success[list[Account]])
+async def get_accounts(country: str | None = None,
+                       status: Status | None = Status.ACTIVE,
+                       ctx: RequestContext = Depends()):
+    response = await forward_request(ctx,
+                                     method="GET",
+                                     url=f"{SERVICE_URL}/v1/accounts",
+                                     params={"country": country,
+                                             "status": status})
+    return response
+
+
 @router.get("/v1/accounts/{account_id}", response_model=Success[Account])
 async def get_account(account_id: int, ctx: RequestContext = Depends()):
     response = await forward_request(ctx,
@@ -36,7 +47,6 @@ async def get_account(account_id: int, ctx: RequestContext = Depends()):
     return response
 
 
-# https://osuakatsuki.atlassian.net/browse/V2-59
 @router.patch("/v1/accounts/{account_id}", response_model=Success[Account])
 async def partial_update_account(account_id: int,
                                  args: AccountUpdate,
@@ -49,4 +59,16 @@ async def partial_update_account(account_id: int,
                                      method="PATCH",
                                      url=f"{SERVICE_URL}/v1/accounts/{account_id}",
                                      json=args.dict())
+    return response
+
+
+@router.delete("/v1/accounts/{account_id}", response_model=Success[Account])
+async def delete_account(account_id: int,
+                         token: HTTPCredentials = Depends(oauth2_scheme),
+                         ctx: RequestContext = Depends()):
+    session = await authenticate(ctx, token.credentials)
+
+    response = await forward_request(ctx,
+                                     method="DELETE",
+                                     url=f"{SERVICE_URL}/v1/accounts/{account_id}")
     return response
